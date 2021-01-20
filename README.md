277f56296153b5f2c73ca7551ed1c059adc233b8
Nikola-Ver <nikolveresh@gmail.com>
2021-01-20 09:50:32 +0300

    innit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FileWatcher
+
+It's simple program to common your files in one.
+
+Let's say you have a folder with your expenses (which are recorded by files) and it would be more convenient for you to output them into one common file. This program can help to do this, for this it is enough to change the paths to files in the file "path" ("path.json"), instead of "path from" specify the path to the folder from which you want to receive information, for example "D:\\expenses", and instead of "path to" the path to the folder in which you would like the program to output a common file, for example "D:\\".
+To start the program, once click on "start" ("start.bat"), and if you want to install autorun, move the program itself to the C drive, then follow the path "C:\\FileWatcher\\start" and find the file there "FileWatcher" ("FileWatcher.vbs"), press the key combination "Win + R", then enter the command "shell: startup", move the file "FileWatcher" ("FileWatcher.vbs") to this folder.
+
+To run the program, you need to <a href="https://nodejs.org/en/download/">install Node js</a>.
---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const [fromPath, toPath] = require('./path.json'); 
+
+// Old version (there is one big problem: if you save the file twice, you will have a repetition)
+// fs.watch(path, (eventType, filename) => {
+//     if (filename && filename !== 'Common.txt' && !/copy/i.exec(filename)) {
+//         try {
+//             if (eventType === 'change') {
+//                 const changedFilePath = `${path}${filename}`; 
+//                 const data = fs.readFileSync(changedFilePath);
+//                 const commonFilePath = `${path}Common.txt`;
+//                 fs.appendFileSync(commonFilePath, data);
+//             }
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     }
+//   });
+
+// In new version fixed previous trouble
+fs.watch(fromPath, (eventType, filename) => {
+    if (filename && filename !== 'Common.txt' && !/copy/i.exec(filename)) {
+        try {
+            fs.readdir(fromPath, (err, files) => {
+                let allData = '';
+                files.forEach(file => {
+                    if (file !== 'Common.txt') {
+                        const filePath = `${fromPath}${file}`;
+                        allData += fs.readFileSync(filePath);
+                    }
+                });
+                const commonFilePath = `${toPath}Common.txt`;
+                fs.writeFileSync(commonFilePath, allData);
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+  });
