62d757d57c9bda405e97d88a413684642181c6e2
Nikola-Ver <nikolveresh@gmail.com>
2021-02-09 14:09:37 +0300

    update

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 const fs = require('fs');
-const [fromPath, toPath] = require('./path.json'); 
+const path = require('path');
+const [fromPath, toPath] = require('./path.json');
 
-// Old version (there is one big problem: if you save the file twice, you will have a repetition)
-// fs.watch(path, (eventType, filename) => {
-//     if (filename && filename !== 'Common.txt' && !/copy/i.exec(filename)) {
-//         try {
-//             if (eventType === 'change') {
-//                 const changedFilePath = `${path}${filename}`; 
-//                 const data = fs.readFileSync(changedFilePath);
-//                 const commonFilePath = `${path}Common.txt`;
-//                 fs.appendFileSync(commonFilePath, data);
-//             }
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
-//   });
+function dateToFileName(date) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    return `${day < 10 ? '0' + day.toString() : 
+              day.toString()}.${month < 10 ? '0' + 
+              month.toString() : 
+              month.toString()}.${year.toString()}.txt`
+}
 
-// In new version fixed previous trouble
 fs.watch(fromPath, (eventType, filename) => {
     try {
-        if (filename && filename !== 'Common.txt' && !/copy/i.exec(filename)) {
-            fs.readdir(fromPath, (err, files) => {
-                let allData = '';
-                files.forEach(file => {
-                    if (file !== 'Common.txt') {
-                        const filePath = `${fromPath}${file}`;
-                        allData += fs.readFileSync(filePath);
-                    }
+        if (filename && filename !== 'Common.txt') {
+            if (/copy/i.exec(filename) && 
+                /([0-9]{1,2}\.){2}[0-9]{1,4}/i.exec(filename)
+            ) {
+                const today = new Date();
+                const newFileName = dateToFileName(today);
+                
+                if (!fs.existsSync(path.join(fromPath, newFileName)) &&
+                    fs.existsSync(fromPath, filename)
+                ) {
+                    fs.rename(
+                        path.join(fromPath, filename), 
+                        path.join(fromPath, newFileName), 
+                        () => {
+                            console.log(`File renamed`);
+                        }
+                    );
+                }
+            } else {
+                fs.readdir(fromPath, (err, files) => {
+                    let allData = '';
+                    files.forEach(file => {
+                        if (file !== 'Common.txt') {
+                            const filePath = path.join(fromPath, file);
+                            allData += fs.readFileSync(filePath);
+                        }
+                    });
+                    const commonFilePath = path.join(toPath, 'Common.txt');
+                    fs.writeFileSync(commonFilePath, allData);
                 });
-                const commonFilePath = `${toPath}Common.txt`;
-                fs.writeFileSync(commonFilePath, allData);
-            });
+            }
         }
     } catch (e) {
         console.log(e);
     }
-  });
+});
